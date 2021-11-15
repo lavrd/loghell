09139c139f141c5ae8ea7d1e763f4210fc465754
Sergei Lavrentev <23312691+lavrxxx@users.noreply.github.com>
2021-11-15 15:47:07 +0300

    Improved rust prototype

--- prototype-rs/src/main.rs
+++ prototype-rs/src/main.rs
@@ -1,8 +1,7 @@
-use tantivy::{Index, TantivyError, ReloadPolicy, schema::{Schema, STORED, FAST, TEXT}, query::{QueryParser}, collector::{TopDocs}, IndexSettings, IndexSortByField, Order};
+use tantivy::{Index, TantivyError, ReloadPolicy, schema::{Schema, STORED, FAST, TEXT}, query::{QueryParser}, collector::{TopDocs}};
 use serde::Serialize;
 use serde_json;
 use chrono::Local;
-use tantivy::store::Compressor;
 
 #[derive(Serialize)]
 struct Data {
@@ -11,7 +10,7 @@ struct Data {
     time: String,
 }
 
-// TODO: how to sort and page queried documents?
+// TODO: how to sort queried documents ASC?
 
 fn main() -> Result<(), TantivyError> {
     let mut schema_builder = Schema::builder();
@@ -19,8 +18,7 @@ fn main() -> Result<(), TantivyError> {
     schema_builder.add_text_field("name", TEXT | STORED);
     schema_builder.add_text_field("time", TEXT | STORED);
     let schema = schema_builder.build();
-    let settings = IndexSettings { sort_by_field: Some(IndexSortByField { field: "id".to_string(), order: Order::Asc }), docstore_compression: Compressor::default() };
-    let index = Index::builder().schema(schema.clone()).settings(settings).create_in_ram()?;
+    let index = Index::create_in_ram(schema.clone());
     let mut index_writer = index.writer(100_000_000)?;
 
     for i in 1..10 {
@@ -39,12 +37,15 @@ fn main() -> Result<(), TantivyError> {
         .reload_policy(ReloadPolicy::OnCommit)
         .try_into()?;
     let searcher = reader.searcher();
-    // let id = index.schema().get_field("id").expect("failed to get id field");
+    let id = index.schema().get_field("id").expect("failed to get id field");
     let name = index.schema().get_field("name").expect("failed to get name field");
     let query_parser = QueryParser::for_index(&index, vec![name]);
     let query = query_parser.parse_query("name-")?;
 
-    let top_docs = searcher.search(&query, &TopDocs::with_limit(10))?;
+    let top_docs = searcher.search(&query,
+                                   &TopDocs::with_limit(1).
+                                       and_offset(0).
+                                       order_by_u64_field(id))?;
     for (_score, doc_address) in top_docs {
         let retrieved_doc = searcher.doc(doc_address)?;
         println!("{}", schema.to_json(&retrieved_doc));
