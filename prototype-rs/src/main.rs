9cf41d031e85d6aea72353ae96a08aa58c4f71dc
Sergei Lavrentev <23312691+lavrxxx@users.noreply.github.com>
2021-11-15 21:18:12 +0300

    Finished rust prototype

--- prototype-rs/src/main.rs
+++ prototype-rs/src/main.rs
@@ -1,22 +1,24 @@
-use tantivy::{Index, TantivyError, ReloadPolicy, schema::{Schema, STORED, FAST, TEXT}, query::{QueryParser}, collector::{TopDocs}};
 use serde::Serialize;
 use serde_json;
 use chrono::Local;
+use tantivy::schema::{FAST, Schema, STORED, TEXT};
+use tantivy::{DocId, Index, ReloadPolicy, SegmentReader, TantivyError};
+use tantivy::collector::TopDocs;
+use tantivy::fastfield::FastFieldReader;
+use tantivy::query::QueryParser;
 
 #[derive(Serialize)]
 struct Data {
     id: u64,
     name: String,
-    time: String,
+    time: i64,
 }
-
-// TODO: how to sort queried documents ASC?
 
 fn main() -> Result<(), TantivyError> {
     let mut schema_builder = Schema::builder();
-    schema_builder.add_u64_field("id", FAST | STORED);
+    schema_builder.add_u64_field("id", STORED);
     schema_builder.add_text_field("name", TEXT | STORED);
-    schema_builder.add_text_field("time", TEXT | STORED);
+    schema_builder.add_i64_field("time", FAST | STORED);
     let schema = schema_builder.build();
     let index = Index::create_in_ram(schema.clone());
     let mut index_writer = index.writer(100_000_000)?;
@@ -25,7 +27,7 @@ fn main() -> Result<(), TantivyError> {
         let data = serde_json::to_string(&Data {
             id: i,
             name: format!("name-{}", i),
-            time: Local::now().to_rfc3339(),
+            time: Local::now().timestamp_nanos(),
         })?;
         let doc = index.schema().parse_document(data.as_str())?;
         index_writer.add_document(doc);
@@ -37,15 +39,22 @@ fn main() -> Result<(), TantivyError> {
         .reload_policy(ReloadPolicy::OnCommit)
         .try_into()?;
     let searcher = reader.searcher();
-    let id = index.schema().get_field("id").expect("failed to get id field");
     let name = index.schema().get_field("name").expect("failed to get name field");
+    let time = index.schema().get_field("time").expect("failed to get time field");
     let query_parser = QueryParser::for_index(&index, vec![name]);
     let query = query_parser.parse_query("name-")?;
 
-    let top_docs = searcher.search(&query,
-                                   &TopDocs::with_limit(1).
-                                       and_offset(0).
-                                       order_by_u64_field(id))?;
+    let top_docs_order_by_id_asc = TopDocs
+    ::with_limit(10).and_offset(0)
+        .custom_score(move |segment_reader: &SegmentReader| {
+            let time_reader = segment_reader.fast_fields().i64(time).unwrap();
+            move |doc: DocId| {
+                let time = time_reader.get(doc);
+                std::cmp::Reverse(time)
+            }
+        });
+    let top_docs = searcher.search(&query, &top_docs_order_by_id_asc)?;
+
     for (_score, doc_address) in top_docs {
         let retrieved_doc = searcher.doc(doc_address)?;
         println!("{}", schema.to_json(&retrieved_doc));
