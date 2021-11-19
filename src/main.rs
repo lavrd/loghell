c63c4e91ac7b82b6dc7d45f89cf90f32a9bfa0a9
Sergei Lavrentev <23312691+lavrxxx@users.noreply.github.com>
2021-11-20 02:48:00 +0300

    Added logger and tokio to accept tcp connections

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,64 @@
-fn main() {
-    println!("Hello, world!");
+use env_logger::Env;
+use log::{debug, error, info};
+use std::env;
+use std::str::from_utf8;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::net::TcpListener;
+
+const DEFAULT_LOG_LEVEL: &str = "DEBUG";
+const DEFAULT_SOCKET_ADDR: &str = "127.0.0.1:0";
+
+const ENV_SOCKET_ADDR: &str = "SOCKET_ADDR";
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    env_logger::Builder::from_env(Env::default().default_filter_or(DEFAULT_LOG_LEVEL)).init();
+
+    let addr = env::var(ENV_SOCKET_ADDR).unwrap_or_else(|_| DEFAULT_SOCKET_ADDR.to_string());
+    let listener = TcpListener::bind(addr).await?;
+    let local_addr = listener.local_addr()?;
+    info!("socket start at {}", local_addr);
+
+    loop {
+        let (mut socket, socket_addr) = listener.accept().await?;
+        debug!("new client from {}", socket_addr.to_string());
+
+        tokio::spawn(async move {
+            let mut buf = [0; 1024];
+
+            loop {
+                let n = match socket.read(&mut buf).await {
+                    Ok(n) if n == 0 => {
+                        debug!("connection with {} socket closed", socket_addr);
+                        return;
+                    }
+                    Ok(n) if n == 5 && buf[0..n] == [255, 244, 255, 253, 6] => {
+                        debug!("telnet connection with {} socket closed", socket_addr);
+                        return;
+                    }
+                    Ok(n) => {
+                        // We use n-2 to remove /r/n.
+                        let data = match from_utf8(&buf[0..n - 2]) {
+                            Ok(data) => data,
+                            Err(e) => {
+                                error!("failed to convert incoming data to string : {}", e);
+                                return;
+                            }
+                        };
+                        debug!("new data from {} : {:?}", socket_addr, data);
+                        n
+                    }
+                    Err(e) => {
+                        error!("failed to read from socket; err : {:?}", e);
+                        0
+                    }
+                };
+
+                if let Err(e) = socket.write_all(&buf[0..n]).await {
+                    error!("failed to write to socket; err : {:?}", e);
+                    return;
+                }
+            }
+        });
+    }
 }
