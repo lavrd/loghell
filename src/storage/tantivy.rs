69189ac98214fc21887725f424e54a045357febd
Sergei Lavrentev <23312691+lavrxxx@users.noreply.github.com>
2021-12-05 14:52:26 +0300

    Tantivy store and search in progress

--- src/storage/tantivy.rs
+++ src/storage/tantivy.rs
@@ -1,19 +1,21 @@
+use std::collections::HashMap;
 use std::error::Error;
 use std::str::from_utf8;
 use std::time::{SystemTime, UNIX_EPOCH};
 
 use serde::Serialize;
-use tantivy::{Index, IndexWriter};
+use serde_json::Value;
+use tantivy::{Document, Index, IndexWriter};
 use tantivy::schema::{FAST, Schema, STORED, TEXT};
 
 use crate::config::Tantivy as TantivyConfig;
 use crate::storage::Storage;
 
-#[derive(Serialize)]
-struct Record {
-    data: String,
-    time: u64,
-}
+// #[derive(Serialize)]
+// struct Record {
+//     data: Value,
+//     time: u64,
+// }
 
 pub struct Tantivy {
     schema: Schema,
@@ -25,11 +27,11 @@ impl Tantivy {
     pub fn new(config: TantivyConfig) -> Result<Self, Box<dyn Error>> {
         let mut schema_builder = Schema::builder();
 
-        schema_builder.add_u64_field("time", FAST | STORED);
-        schema_builder.add_text_field("data", TEXT | STORED);
-        // for text_field in config.fields.text.iter() {
-        //     schema_builder.add_text_field(&format!("data.{}", text_field), TEXT | STORED);
-        // }
+        schema_builder.add_u64_field("_loghell_time", FAST); // TODO: To const.
+        // schema_builder.add_text_field("data", TEXT | STORED);
+        for text_field in config.fields.text.iter() {
+            schema_builder.add_text_field(text_field, TEXT | STORED);
+        }
 
         let schema = schema_builder.build();
         let index = Index::create_in_ram(schema.clone());
@@ -45,15 +47,17 @@ impl Tantivy {
 
 impl Storage for Tantivy {
     fn store(&mut self, data: &[u8]) -> Result<(), Box<dyn Error>> {
-        let data_as_str = from_utf8(data)?;
-        let now_as_nanos = SystemTime::now().duration_since(UNIX_EPOCH)?.as_secs(); // TODO: We need nanos here.
-        let record = Record {
-            data: data_as_str.to_string(),
-            time: now_as_nanos,
-        };
-        let record_as_str = serde_json::to_string(&record)?;
+        let now_as_nanos_u128 = SystemTime::now().duration_since(UNIX_EPOCH)?.as_nanos();
+        let now_as_nanos_u64 = u64::try_from(now_as_nanos_u128)?;
+        let mut data_as_value: Value = serde_json::from_slice(data)?;
+        data_as_value["_loghell_time"] = serde_json::Value::from(now_as_nanos_u64);
 
-        let doc = self.index.schema().parse_document(&record_as_str)?;
+        println!("{}", data_as_value.to_string());
+
+        let doc = self.index.schema().parse_document(&data_as_value.to_string())?;
+
+        println!("{}", self.schema.to_json(&doc));
+
         self.index_writer.add_document(doc);
         self.index_writer.commit()?;
 
@@ -61,52 +65,81 @@ impl Storage for Tantivy {
     }
 }
 
+// #[cfg(test)]
+// mod tests {
+//     use serde::Serialize;
+//     use tantivy::{DocId, ReloadPolicy, SegmentReader, Term};
+//     use tantivy::collector::TopDocs;
+//     use tantivy::fastfield::FastFieldReader;
+//     use tantivy::query::{QueryParser, TermQuery};
+//     use tantivy::schema::IndexRecordOption;
+//
+//     use crate::config::{Tantivy as TantivyConfig, TantivyFields};
+//     use crate::Storage;
+//     use crate::storage::tantivy::Tantivy;
+//
+//     #[test]
+//     fn whole_logic() {
+//         let config = TantivyConfig {
+//             fields: TantivyFields {
+//                 text: Box::new(["level".to_string(), "message".to_string()]) as Box<[String]>
+//             }
+//         };
+//         let mut tantivy = Tantivy::new(config).unwrap();
+//         tantivy.store(r#"{"level":"debug","message":"log1"}"#.as_bytes()).unwrap();
+//         tantivy.store(r#"{"level":"info","message":"log2"}"#.as_bytes()).unwrap();
+//         tantivy.store(r#"{"level":"error","message":"log3"}"#.as_bytes()).unwrap();
+//         tantivy.store(r#"{"level":"debug","message":"log4"}"#.as_bytes()).unwrap();
+//
+//         let reader = tantivy.index
+//             .reader_builder()
+//             .reload_policy(ReloadPolicy::OnCommit)
+//             .try_into().unwrap();
+//         let searcher = reader.searcher();
+//         let _loghell_time = tantivy.index.schema().get_field("_loghell_time").unwrap();
+//         // let level = tantivy.index.schema().get_field("level").unwrap();
+//         let __field = tantivy.index.schema().get_field("message").unwrap();
+//         // let query_parser = QueryParser::for_index(&tantivy.index, vec![data]);
+//         // let query = query_parser.parse_query("data:level:error").unwrap();
+//         let query = TermQuery::new(
+//             Term::from_field_text(__field, "log3"),
+//             IndexRecordOption::Basic,
+//         );
+//
+//         let top_docs_order_by_id_asc = TopDocs
+//         ::with_limit(10).and_offset(0)
+//             .custom_score(move |segment_reader: &SegmentReader| {
+//                 let time_reader = segment_reader.fast_fields().u64(_loghell_time).unwrap();
+//                 move |doc: DocId| {
+//                     let time = time_reader.get(doc);
+//                     std::cmp::Reverse(time)
+//                 }
+//             });
+//         let top_docs = searcher.search(&query, &top_docs_order_by_id_asc).unwrap();
+//
+//         println!("found {} documents", top_docs.len());
+//         for (_, doc_address) in top_docs {
+//             let retrieved_doc = searcher.doc(doc_address).unwrap();
+//             println!("{:?}", retrieved_doc.field_values());
+//             println!("{:?}", doc_address.doc_id);
+//             println!("{}", tantivy.schema.to_json(&retrieved_doc));
+//         }
+//     }
+// }
+
 #[cfg(test)]
 mod tests {
-    use tantivy::{DocId, ReloadPolicy, SegmentReader};
-    use tantivy::collector::TopDocs;
-    use tantivy::fastfield::FastFieldReader;
-    use tantivy::query::QueryParser;
-
-    use crate::config::{Tantivy as TantivyConfig, TantivyFields};
-    use crate::Storage;
-    use crate::storage::tantivy::Tantivy;
+    use tantivy::schema::{Schema, STORED, TEXT};
 
     #[test]
-    fn whole_logic() {
-        let a: Box<[String]> = Box::new(["level".to_string()]) as Box<[String]>;
-        let config = TantivyConfig { fields: TantivyFields { text: a } };
-        let mut tantivy = Tantivy::new(config).unwrap();
-        tantivy.store("{\"level\":\"debug\"}".as_bytes()).unwrap();
-        tantivy.store("{\"level\":\"info\"}".as_bytes()).unwrap();
-        tantivy.store("{\"level\":\"trace\"}".as_bytes()).unwrap();
-        tantivy.store("{\"level\":\"debug\"}".as_bytes()).unwrap();
+    fn test() {
+        let mut schema_builder = Schema::builder();
+        schema_builder.add_text_field("level", TEXT | STORED);
+        schema_builder.add_text_field("message", TEXT | STORED);
+        let schema = schema_builder.build();
 
-        let reader = tantivy.index
-            .reader_builder()
-            .reload_policy(ReloadPolicy::OnCommit)
-            .try_into().unwrap();
-        let searcher = reader.searcher();
-        let data = tantivy.index.schema().get_field("data").expect("failed to get data field");
-        let time = tantivy.index.schema().get_field("time").expect("failed to get time field");
-        let query_parser = QueryParser::for_index(&tantivy.index, vec![data]);
-        let query = query_parser.parse_query("data.level is level").unwrap(); // TODO: It is not working.
-
-        let top_docs_order_by_id_asc = TopDocs
-        ::with_limit(10).and_offset(0)
-            .custom_score(move |segment_reader: &SegmentReader| {
-                let time_reader = segment_reader.fast_fields().u64(time).unwrap();
-                move |doc: DocId| {
-                    let time = time_reader.get(doc);
-                    std::cmp::Reverse(time)
-                }
-            });
-        let top_docs = searcher.search(&query, &top_docs_order_by_id_asc).unwrap();
-
-        println!("found {} documents", top_docs.len());
-        for (_score, doc_address) in top_docs {
-            let retrieved_doc = searcher.doc(doc_address).unwrap();
-            println!("{}", tantivy.schema.to_json(&retrieved_doc));
-        }
+        let doc = schema.parse_document(r#"{"level":"debug","message":"log"}"#).unwrap();
+        println!("{}", schema.to_json(&doc));
     }
 }
+
